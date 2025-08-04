e19096c28a411a2e37603b01edd327cf2a38e21c
Seba02-sr <ramellasebastian02@gmail.com>
2025-08-04 01:47:30 -0300

    fix(0060): Update banco entity for used auditableEntity

--- services/user-svc/src/main/java/edu/utn/frsf/isi/dan/user/model/Banco.java
+++ services/user-svc/src/main/java/edu/utn/frsf/isi/dan/user/model/Banco.java
@@ -7,10 +7,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Table(name = "bancos")
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@SuperBuilder
 public class Banco extends AuditableEntity {
 
     @Id
