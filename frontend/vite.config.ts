b8c08fa011933b048fb4b15103fb2aac4548c632
Seba02-sr <ramellasebastian02@gmail.com>
2025-08-28 00:57:57 -0300

    feat(200): Added configuration of tailwind + react + scss + postcss + vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), 'src/scss/main.scss'),
+      },
+      output: {
+        entryFileNames: '[name].js', // opcional, si tienes JS
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith('.css')) return 'main.css';
+          return 'assets/[name].[ext]';
+        },
+      },
+    },
+    cssCodeSplit: true,
+  },
+});
