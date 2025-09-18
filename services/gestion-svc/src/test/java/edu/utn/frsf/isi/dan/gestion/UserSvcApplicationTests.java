78df532371ee92309960610640ddb7a828ae64ac
sebastianr <sebastianr@serfe.com>
2025-09-17 23:25:32 -0300

    Use H2 test profile for gestion-svc contextLoads

--- services/gestion-svc/src/test/java/edu/utn/frsf/isi/dan/gestion/UserSvcApplicationTests.java
+++ services/gestion-svc/src/test/java/edu/utn/frsf/isi/dan/gestion/UserSvcApplicationTests.java
@@ -2,8 +2,10 @@ package edu.utn.frsf.isi.dan.gestion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GestionSvcApplicationTests {
 
   @Test
