9bd31f28d095ef319be3a8c864ce3d0736f39149
sebastianr <sebastianr@serfe.com>
2025-09-17 23:29:51 -0300

    mockBean RabbitMQ for run spotless

--- services/gestion-svc/src/test/java/edu/utn/frsf/isi/dan/gestion/UserSvcApplicationTests.java
+++ services/gestion-svc/src/test/java/edu/utn/frsf/isi/dan/gestion/UserSvcApplicationTests.java
@@ -1,7 +1,9 @@
 package edu.utn.frsf.isi.dan.gestion;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
@@ -10,4 +12,7 @@ class GestionSvcApplicationTests {
 
   @Test
   void contextLoads() {}
+
+  // Evita conexiones reales a Rabbit durante el arranque del contexto
+  @MockBean private RabbitTemplate rabbitTemplate;
 }
