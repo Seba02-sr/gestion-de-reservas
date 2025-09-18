0c644f6ed90e90de6555375990d384b1caca4556
sebastianr <sebastianr@serfe.com>
2025-09-17 23:35:23 -0300

    Not real db connections for spotless

--- services/gestion-svc/src/test/java/edu/utn/frsf/isi/dan/gestion/UserSvcApplicationTests.java
+++ services/gestion-svc/src/test/java/edu/utn/frsf/isi/dan/gestion/UserSvcApplicationTests.java
@@ -1,6 +1,8 @@
 package edu.utn.frsf.isi.dan.gestion;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,4 +17,6 @@ class GestionSvcApplicationTests {
 
   // Evita conexiones reales a Rabbit durante el arranque del contexto
   @MockBean private RabbitTemplate rabbitTemplate;
+  @MockBean private ConnectionFactory connectionFactory;
+  @MockBean private AmqpAdmin amqpAdmin;
 }
