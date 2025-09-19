3bb43f8dd2335f57ec9628a695623780d3f56576
niclesanti <niclesanti687@gmail.com>
2025-09-19 16:55:36 -0300

    Se agrego el atributo cerrado en la entidad hotel y su respectiva columna en su tabla

--- services/gestion-svc/src/main/java/edu/utn/frsf/isi/dan/gestion/model/Hotel.java
+++ services/gestion-svc/src/main/java/edu/utn/frsf/isi/dan/gestion/model/Hotel.java
@@ -1,8 +1,10 @@
 package edu.utn.frsf.isi.dan.gestion.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -15,7 +17,6 @@ public class Hotel {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
-
   private String nombre;
   private String cuit;
   private String domicilio;
@@ -24,11 +25,11 @@ public class Hotel {
   private String telefono;
   private String correoContacto;
   private Integer categoria;
-
+  private Boolean cerrado;
   @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "hotel")
   @JsonIgnore
   private List<Habitacion> habitaciones;
-
+  
   @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "hotel")
   private List<AmenityHotel> amenities;
 }
